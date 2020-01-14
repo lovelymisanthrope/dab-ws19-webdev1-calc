894b730a70badfa01a4e9d47fd184eebd6869e12
dimw <dimw@gmx.de>
2020-01-14 11:52:39 +0100

    Parse operands as numbers
    #2

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
 
 var arguments = process.argv.slice(2);
 
-var operand1 = arguments[0];
-var operand2 = arguments[1];
+var operand1 = parseFloat(arguments[0]);
+var operand2 = parseFloat(arguments[1]);
 var operator = arguments[2];
 
 console.log('Calculating:', operand1, operand2, operator);
