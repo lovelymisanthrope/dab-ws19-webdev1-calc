19d482580e83b38d3d58c2f53c985e4d58e7970e
Rebecca <rebecca.buchheit@gmx.de>
2020-01-20 18:20:08 +0100

    test

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 /**
  * Reverse Polish Notation Calculator
  */
+const arguments = process.argv.slice(2);
 
-var arguments = process.argv.slice(2);
+const operand1 = parseFloat(arguments[0]);
+const operand2 = parseFloat(arguments[1]);
 
-var operand1 = parseFloat(arguments[0]);
-var operand2 = parseFloat(arguments[1]);
-
-var operator = arguments[2];
+const operator = arguments[2];
 
 console.log('Calculating:', operand1, operand2, operator);
 
-var result;
+let result;
 
 switch (operator) {
   case '+':
